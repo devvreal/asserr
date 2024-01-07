38b17359bd0cb74e35e80458ce2079eb9251585b
devv <121615842+devvreal@users.noreply.github.com>
2024-01-07 20:34:46 +0100

    Update 77_DOZPB.lua

--- 77_DOZPB.lua
+++ 77_DOZPB.lua
@@ -285,63 +285,97 @@ local function AutoSpam()
             end
         end)
 
-        while getgenv().DetectSpam do
-            task.wait()
-            local Ball = Balls:FindFirstChildOfClass("Part")
-            local plrRP = get_plrRP()
-            local ProxyPlayer = get_ProxyPlayer()
-
-            if not Ball then
-                getgenv().SpamClickA = false
-            end
-
-            if Ball and Ball:GetAttribute("realBall") and OldBall ~= Ball then
-                Ball.Changed:Connect(function()
+        local function DetectSpam()
+            local Balls = workspace:WaitForChild("Balls", 20)
+        
+            local OldPos = Vector3.new()
+            local OldTick1 = tick()
+        
+            local OldBall = Balls
+            local TargetPlayer = ""
+            local SpamNum = 0
+            local BallSpeed = 0
+            local BallDistance = 0
+        
+            task.spawn(function()
+                local OldTick = tick()
+                local OldPos = Vector3.new()
+                while getgenv().DetectSpam do
                     task.wait()
+                    local plrRP = get_plrRP()
                     local Ball = Balls:FindFirstChildOfClass("Part")
-
-                    if Ball then
-                        TargetPlayer = Ball:GetAttribute("target")
-
-                        if ProxyPlayer and TargetPlayer == ProxyPlayer.Name or get_plr() and TargetPlayer == get_plr().Name then
-                            SpamNum = SpamNum + 1
-                            if SpamNum >= 3 then
-                                getgenv().SpamClickA = true
-                            end
-                        else
-                            SpamNum = 0
-                        end
-
-                        local args = ProxyPlayer and ProxyPlayer:FindFirstChild("HumanoidRootPart")
-                        local HL1 = ProxyPlayer and ProxyPlayer:FindFirstChild("Highlight")
-                        local HL2 = get_plrChar() and get_plrChar():FindFirstChild("Highlight")
-
-                        if plrRP and HL1 and args or plrRP and HL2 and args then
-                            local DistancePlayer = (ProxyPlayer.HumanoidRootPart.Position - plrRP.Position).Magnitude
-                            local DistanceBall = (Ball.Position - plrRP.Position).Magnitude
-
-                            if get_PlayersNumber() < 3 then
-                                if DistancePlayer <= 30 and DistanceBall <= 35 then
-                                    getgenv().SpamClickA = true
-                                else
-                                    getgenv().SpamClickA = false
-                                end
-                            else
-                                if DistancePlayer <= 30 and DistanceBall <= 35 then
-                                    getgenv().SpamClickA = true
-                                else
-                                    getgenv().SpamClickA = false
-                                end
-                            end
-                        else
-                            getgenv().SpamClickA = false
+                    if plrRP and Ball then
+                        BallDistance = (plrRP.Position - Ball.Position).Magnitude
+                        BallSpeed = (OldPos - Ball.Position).Magnitude
+                        if tick() - OldTick >= 1 / 60 then
+                            OldTick = tick()
+                            OldPos = Ball.Position
                         end
                     end
-                end)
-                OldBall = Ball
+                end
+            end)
+        
+            while getgenv().DetectSpam do
+                task.wait()
+                local Ball = Balls:FindFirstChildOfClass("Part")
+                local plrRP = get_plrRP()
+                local ProxyPlayer = get_ProxyPlayer()
+        
+                if not Ball or not plrRP or not plrRP.Parent:FindFirstChild("Humanoid") or plrRP.Parent.Humanoid.Health <= 0 then
+                    getgenv().SpamClickA = false
+                    SpamNum = 0
+                    task.wait(1) 
+                    OldBall = Balls  
+                    continue
+                end
+        
+                if Ball and Ball:GetAttribute("realBall") and OldBall ~= Ball then
+                    Ball.Changed:Connect(function()
+                        task.wait()
+                        local Ball = Balls:FindFirstChildOfClass("Part")
+        
+                        if Ball then
+                            TargetPlayer = Ball:GetAttribute("target")
+        
+                            if ProxyPlayer and TargetPlayer == ProxyPlayer.Name or get_plr() and TargetPlayer == get_plr().Name then
+                                SpamNum = SpamNum + 1
+                                if SpamNum >= 3 then
+                                    getgenv().SpamClickA = true
+                                end
+                            else
+                                SpamNum = 0
+                            end
+        
+                            local args = ProxyPlayer and ProxyPlayer:FindFirstChild("HumanoidRootPart")
+                            local HL1 = ProxyPlayer and ProxyPlayer:FindFirstChild("Highlight")
+                            local HL2 = get_plrChar() and get_plrChar():FindFirstChild("Highlight")
+        
+                            if plrRP and HL1 and args or plrRP and HL2 and args then
+                                local DistancePlayer = (ProxyPlayer.HumanoidRootPart.Position - plrRP.Position).Magnitude
+                                local DistanceBall = (Ball.Position - plrRP.Position).Magnitude
+        
+                                if get_PlayersNumber() < 3 then
+                                    if DistancePlayer <= 30 and DistanceBall <= 35 then
+                                        getgenv().SpamClickA = true
+                                    else
+                                        getgenv().SpamClickA = false
+                                    end
+                                else
+                                    if DistancePlayer <= 30 and DistanceBall <= 35 then
+                                        getgenv().SpamClickA = true
+                                    else
+                                        getgenv().SpamClickA = false
+                                    end
+                                end
+                            else
+                                getgenv().SpamClickA = false
+                            end
+                        end
+                    end)
+                    OldBall = Ball
+                end
             end
         end
-    end
 
     getgenv().DetectSpam = true
     DetectSpam()
@@ -349,7 +383,7 @@ end
 
 local Ap = Tabs.Main:AddToggle("Ap", {
     Title = "Auto Parry",
-    Description = "Parries the ball every time the user is being targetded ",
+    Description = "Parries the ball every time the user is being targeted.",
     Default = false 
 })
 
@@ -360,7 +394,7 @@ end)
 
 local As = Tabs.Main:AddToggle("As", {
     Title = "Auto Spam",
-    Description = "Automatically starts spamming based on the ball speed ",
+    Description = "Automatically starts spamming based on the ball speed.",
     Default = false 
 })
 
