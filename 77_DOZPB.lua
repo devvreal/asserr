e7d8d89d80a4371f7e03d29207adc0405aca6b44
devv <121615842+devvreal@users.noreply.github.com>
2024-01-18 19:16:01 +0100

    Update 77_DOZPB.lua

--- 77_DOZPB.lua
+++ 77_DOZPB.lua
@@ -1,45 +1,24 @@
-local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
 local Remotes = ReplicatedStorage:WaitForChild("Remotes", 9e9)
-local localPlayer = Players.LocalPlayer
+local localPlayer = game.Players.LocalPlayer
 
-local character = localPlayer.Character or localPlayer.CharacterAdded:Wait()
+local function ParryNearbyBall()
+    local character = localPlayer.Character
+    if not character then return end
+    
+    local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+    if not humanoidRootPart then return end
 
-local uigrad2 = character:WaitForChild("Humanoid"):WaitForChild("HumanoidDisplay").Parry.UIGradient
-local heartbeatConnection
-
--- Adjust the radius of the detection
-local detectionRadius = 20
-
--- Function to check if a ball is within the detection radius
-local function isBallDetected(ball, playerPosition)
-    local ballPosition = ball.Position
-    local distance = (playerPosition - ballPosition).magnitude
-    return distance <= detectionRadius
-end
-
--- Function to parry the ball
-local function ParryBall()
-    Remotes:WaitForChild("ParryButtonPress"):Fire()
-    Remotes:WaitForChild("ParryButtonPress"):Fire() -- Adjust the number of times to spam the parry button
-end
-
--- Connect the functions to the Heartbeat event
-heartbeatConnection = game:GetService("RunService").Heartbeat:Connect(function()
-    local playerPosition = character.HumanoidRootPart.Position
     local balls = workspace:WaitForChild("Balls") -- Change "Balls" to the actual name of the ball part
     
     for _, ball in pairs(balls:GetChildren()) do
-        if ball:IsA("Part") and isBallDetected(ball, playerPosition) then
-            ParryBall()
+        if ball:IsA("Part") and (ball.Position - humanoidRootPart.Position).magnitude <= 10 then
+            Remotes:WaitForChild("ParryButtonPress"):Fire()
+            Remotes:WaitForChild("ParryButtonPress"):Fire() -- Adjust the number of times to spam the parry button
+            return
         end
     end
-end)
+end
 
--- Additional: Use UserInputService to immediately detect the ball when clicked
-UserInputService.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 then
-        ParryBall()
-    end
-end)
+RunService.Heartbeat:Connect(ParryNearbyBall)
