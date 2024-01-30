4440dd5bf3bc9933de3fd601cf9ab872743029e8
devv <121615842+devvreal@users.noreply.github.com>
2024-01-30 20:43:05 +0100

    Update Untitled-2.lua

--- Untitled-2.lua
+++ Untitled-2.lua
@@ -1,9 +1,5 @@
 local WEBHOOK_URL = "https://discord.com/api/webhooks/1125605955258814534/YoXLF6b3SOcks0yMgnV5RwGWSJty0W_0eKS9ouv8TXrkgZvNG_YzZQVCn0oSxdy7wwEV"
 
-local function getHWID()
-    return game:HttpGet("https://api.roblox.com/marketplace/productinfo?assetId=" .. game.GameId).ProductId
-end
-
 local function sendToDiscord(message)
     local data = {
         content = message,
@@ -18,14 +14,13 @@ local function sendToDiscord(message)
     end)
 
     if success then
-        print("d sent to Discord successfully ")
+        print("Message sent to Discord successfully.")
     else
-        warn("Failed to send message to Discord ", response)
+        warn("Failed to send message to Discord:", response)
     end
 end
 
- 
 local playerName = game.Players.LocalPlayer.Name
-local hwid = getHWID()
-local message = "Player " .. playerName .. " executed! HWID: " .. hwid
+local userId = game.Players.LocalPlayer.UserId
+local message = "Player " .. playerName .. " executed! UserId: " .. userId
 sendToDiscord(message)
